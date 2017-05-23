f588c7a1012364588eb7d0e156db2853887e5538
hdicksonjr <hcdicksonjr@gmail.com>
2017-05-23 08:59:23 -0400

    add first test coverage

---
+++ test/test-data.js
@@ -0,0 +1,14 @@
+var testData = [
+  { url: "http://2.bp.blogspot.com/-p55jA7cqmXQ/VTbWebhZ2uI/AAAAAAAABWM/LcWWrHJ8N0I/s1600/WHitewater%2Bkayak%2Becuador%2BboofG1.jpg",
+   title: "BOOOF!!",
+   caption: "this is a picture of someone boofing",
+  },
+  { url: "http://jacksonkayak.com/wp-content/uploads/2013/03/860618_344403012345717_1786279341_o.jpg",
+    title: "GREAT boof!",
+    caption: "great job, bud!",
+  },
+  { url: "http://www.wetplanetwhitewater.com/wp-content/uploads/2014/06/boofing_clinic_whitesalmon_1.jpg",
+    title: "solid boof",
+    caption: "classic form",
+  },
+]
